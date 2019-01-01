16e296f6656af76c6036ef06c66e33f489b3be49
Floo <florian.chaudret@gmail.com>
2019-01-01 21:37:20 +0100

    first

---
+++ README.md
@@ -0,0 +1,22 @@
+## Quick start
+``` BASH
+cp .env.example .env
+npm i
+npm run start
+```
+go: http://localhost:3000/
+
+
+## Debug
+### config ide
+![ide config](./doc/ide.png)
+
+### builder watch
+``` BASH
+cp .env.example .env
+npm i
+npm run build-development
+```
+
+### Debug
+![ide config](./doc/debug.png)
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const Dotenv = require('dotenv-webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    entry: require('path').join(__dirname, '/src'),
+    target: 'node',
+    output: {
+        path: require('path').join(__dirname, '/dist'),
+        filename: 'server.js'
+    },
+    devtool: "source-map",
+    plugins: [
+        new Dotenv({
+            path: '.env',
+            safe: true
+        })
+    ],
+    externals: [nodeExternals()]
+};
